71041b4db7447f79fcafd5fce3a4161623bd1c4a
irfanplazuardi <irfanplazuardi@gmail.com>
2025-08-09 07:26:29 +0800

    update fix browser2

--- playwright.config.ts
+++ playwright.config.ts
@@ -60,8 +60,8 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices["Desktop Chrome"], channel: "chrome" },
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     },
 
     // {
